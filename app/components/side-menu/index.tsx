6915ea8b279354a29b51c12c534e80c7ff4f4a9a
adam <pasierb.adam@gmail.com>
2024-04-25 08:45:43 +0200

    add side menu component

---
+++ app/components/side-menu/index.tsx
@@ -0,0 +1,7 @@
+export default function SideMenu() {
+  return (
+    <div className="fixed bottom-0 flex flex-col sm:left-[50px] xl:left-[100px] hidden sm:block">
+      <div className="bg-secondaryText w-[1px] h-52"></div>
+    </div>
+  );
+}
