9331159c355fed742d22940e30290a94e661f30a
adam <pasierb.adam@gmail.com>
2024-05-09 11:11:03 +0200

    timeline begin

--- components/expedu/index.tsx
+++ components/expedu/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import SimpleHeading from "../simple-heading";
 
+import { timelineMock } from "../../mocks";
+
 const ExpEdu = () => {
   return (
     <div id="expedu">
       <SimpleHeading label="Experience and Education" />
+      <div className="timeline">
+        {timelineMock.map((checkpoint) => (
+          <div key={checkpoint.id} className="checkpoint-wrapper">
+            <div className="checkpoint-connector">
+              <div className="checkpoint-connector-line" />
+              <div className="checkpoint-connector-arrow" />
+            </div>
+            <div className="checkpoint">
+              {checkpoint.techs && (
+                <div className="techs flex">
+                  {checkpoint.techs.map((tech) => (
+                    <div className="tech" key={tech.id}>
+                      {tech.name}
+                    </div>
+                  ))}
+                </div>
+              )}
+              <p className="checkpoint-title">{checkpoint.title}</p>
+              <p className="checkpoint-date">{`${checkpoint.date.from} - ${checkpoint.date.to}`}</p>
+              <p className="checkpoint-description">{checkpoint.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ mocks/index.js
@@ -0,0 +1,41 @@
+const timelineMock = [
+  {
+    id: 1,
+    type: "exp",
+    title: "Title 1",
+    description: "Description 1",
+    date: {
+      from: "10-10-2015",
+      to: "10-10-2015",
+    },
+    techs: [
+      {
+        id: 1,
+        name: "Tech 1",
+        key: "tech1",
+      },
+      {
+        id: 2,
+        name: "Tech 2",
+        key: "tech1",
+      },
+      {
+        id: 3,
+        name: "Tech 3",
+        key: "tech1",
+      },
+    ],
+  },
+  {
+    id: 2,
+    type: "eu",
+    title: "Title 1",
+    description: "Description 1",
+    date: {
+      from: "10-10-2015",
+      to: "10-10-2015",
+    },
+  },
+];
+
+export { timelineMock };
