5b5905a3a7cad48d04a3a1f34c0575c5c4a57322
adam <pasierb.adam@gmail.com>
2024-05-12 12:00:31 +0200

    somewhere towards timeline

--- components/expedu/index.tsx
+++ components/expedu/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 import SimpleHeading from "../simple-heading";
 
@@ -5,33 +6,12 @@ import { timelineMock } from "../../mocks";
 
 const ExpEdu = () => {
   return (
-    <div id="expedu">
+    <section id="expedu" className="pb-8">
       <SimpleHeading label="Experience and Education" />
       <div className="timeline">
-        {timelineMock.map((checkpoint) => (
-          <div key={checkpoint.id} className="checkpoint-wrapper">
-            <div className="checkpoint-connector">
-              <div className="checkpoint-connector-line" />
-              <div className="checkpoint-connector-arrow" />
-            </div>
-            <div className="checkpoint">
-              {checkpoint.techs && (
-                <div className="techs flex">
-                  {checkpoint.techs.map((tech) => (
-                    <div className="tech" key={tech.id}>
-                      {tech.name}
-                    </div>
-                  ))}
-                </div>
-              )}
-              <p className="checkpoint-title">{checkpoint.title}</p>
-              <p className="checkpoint-date">{`${checkpoint.date.from} - ${checkpoint.date.to}`}</p>
-              <p className="checkpoint-description">{checkpoint.description}</p>
-            </div>
-          </div>
-        ))}
+        {timelineMock.map((checkpoint, index) => )}
       </div>
-    </div>
+    </section>
   );
 };
 
